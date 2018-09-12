df22c66a40c19783cb7f19e2dc0223702d7b8e2a
Gabriel Kuettel <gabrielkuettel@gmail.com>
2018-09-11 20:01:17 -0600

    add profile page

--- public/assets/javascript/browse.js
+++ public/assets/javascript/browse.js
@@ -5,16 +5,13 @@ database.ref().on("child_added", function (Snapshot){
     var lastDate = Snapshot.val().pet.lastDate;
     var petType = Snapshot.val().pet.petType;
     var breed = Snapshot.val().pet.breed;
-    
-
-
+            
     console.log(petName);
     console.log(lastLoc);
     console.log(lastDate);
     console.log(petType);
     console.log(breed);
     
-
     // Create the new row
     var newPet = $("<tr>").append(
         $("<td>").text(petName),
