9f961dc13a835ff00eb6692cab7e01665a5b3cb3
Gabriel Kuettel <gabrielkuettel@gmail.com>
2018-09-13 00:33:01 -0600

    add facebook auth

--- public/assets/javascript/firebase/authFirebase.js
+++ public/assets/javascript/firebase/authFirebase.js
@@ -1,7 +1,48 @@
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'profile.html',
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            var user = authResult.user;
+            var credential = authResult.credential;
+            var isNewUser = authResult.additionalUserInfo.isNewUser;
+            var providerId = authResult.additionalUserInfo.providerId;
+            var operationType = authResult.operationType;
+            // Do something with the returned AuthResult.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        signInFailure: function (error) {
+            // Some unrecoverable error occurred during sign-in.
+            // Return a promise when error handling is completed and FirebaseUI
+            // will reset, clearing any UI. This commonly occurs for error code
+            // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+            // occurs. Check below for more details on this.
+            return handleUIError(error);
+        },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            // document.getElementById('loader').style.display = 'none';
+        }
+    },
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function () {
+        window.location.assign('<your-privacy-policy-url>');
+    }
+};
+
 
 var userID = '';
 
@@ -12,6 +53,7 @@ function endSession() {
         console.error('Sign Out Error', error);
     });
 }
+
 
 initApp = function () {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -51,9 +93,17 @@ window.addEventListener('load', function () {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             userID = firebase.auth().currentUser.uid;
+            provData = user.providerData[0].uid;
             console.log('UserID: ' + userID);
+            console.log ('Provider data: ' + provData);
+            console.log(user);
         } else {
             console.log('err: not logged in?');
         }
     });
 });
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
